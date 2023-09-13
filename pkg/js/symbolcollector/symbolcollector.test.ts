9bfe0175263029fb0279ac8e5b82ba94525833b9
Daniel Jeffery <danielajeffery@gmail.com>
2023-09-13 07:49:23 -0700

    feat: First cut of autocompletion suggestions

---
+++ pkg/js/symbolcollector/symbolcollector.test.ts
@@ -0,0 +1,37 @@
+import { generateSymbols, getSuggestions } from "./index";
+
+describe("generateSymbols", () => {
+  const dsl = `model
+  schema 1.1
+type user
+type team
+  relations
+    define member: [user,team#member]
+type repo
+  relations
+    define admin: [user,team#member] or repo_admin from owner
+    define maintainer: [user,team#member] or admin
+    define owner: [organization]
+    define reader: [user,team#member] or triager or repo_reader from owner
+    define triager: [user,team#member] or writer
+    define writer: [user,team#member] or maintainer or repo_writer from owner
+type organization
+  relations
+    define member: [user] or owner
+    define owner: [user]
+    define repo_admin: [user,organization#member]
+    define repo_reader: [user,organization#member]
+    define repo_writer: [user,organization#member]`;
+
+  it(`Visits tree and generates all symbols`, () => {
+    const result = generateSymbols(dsl);
+    expect(result).toMatchSnapshot();
+  });
+
+  it(`Visits tree and autocomplete text`, () => {
+    const result = getSuggestions(dsl, 5, 3);
+    expect(result).toMatchSnapshot()
+  });
+});
+
+

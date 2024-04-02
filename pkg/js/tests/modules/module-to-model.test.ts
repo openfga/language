65c8d6fcaa9974d7bb36b95a86cc6d1de8fede92
Ewan Harris <ewan.harris@okta.com>
2024-04-02 11:20:36 +0100

    test(js): run semantic validation test cases against transform module

--- pkg/js/tests/modules/module-to-model.test.ts
+++ pkg/js/tests/modules/module-to-model.test.ts
@@ -6,11 +6,11 @@ import {
   ModuleTransformationSingleError,
 } from "../../errors";
 import { transformModuleFilesToModel } from "../../transformer/modules/modules-to-model";
-import { loadModuleTestCases } from "../_testcases";
+import { loadDSLValidationErrorTestCases, loadModuleTestCases } from "../_testcases";
 
-describe("transformModuleFilesToModel", () => {
-  const testCases = loadModuleTestCases();
-  testCases.forEach((testCase) => {
+describe("transformModuleFilesToModel - module test cases", () => {
+  const moduleTestCases = loadModuleTestCases();
+  moduleTestCases.forEach((testCase) => {
     const testFn = testCase.skip ? it.skip : it;
 
     testFn(`transformModuleFilesToModel ${testCase.name}`, () => {
@@ -54,6 +54,60 @@ describe("transformModuleFilesToModel", () => {
     });
   });
 
+  const semanticValidationTestCases = loadDSLValidationErrorTestCases();
+  semanticValidationTestCases.forEach((testCase) => {
+    let testFn = testCase.skip ? it.skip : it;
+
+    // Skip any test case already using module as it won't be valid
+    if (testCase.dsl.trim().startsWith("module")) {
+      testFn = it.skip;
+    }
+
+    // Skip schema validation errors
+    if (testCase.dsl.includes("0.9")) {
+      testFn = it.skip;
+    }
+
+    testFn(`transformModuleFilesToModel ${testCase.name}`, () => {
+      try {
+        const dsl = testCase.dsl.replace(/model\n {2}schema 1\.1/, "module test\n");
+        transformModuleFilesToModel([{ name: "test.fga", contents: dsl }], "1.2");
+      } catch (error) {
+        expect(error).toBeInstanceOf(ModuleTransformationError);
+        const exception = error as ModuleTransformationError;
+
+        const errorsCount = testCase.expected_errors.length;
+        expect(exception.message).toEqual(
+          `${errorsCount} error${errorsCount === 1 ? "" : "s"} occurred:\n\t* ${testCase.expected_errors
+            .map((err: ModelValidationSingleError | ModuleTransformationSingleError | DSLSyntaxSingleError) => {
+              let errorType = "transformation-error";
+              if ((err as ModelValidationSingleError).metadata?.errorType) {
+                errorType = (err as ModelValidationSingleError).metadata!.errorType;
+              } else if ((err as DSLSyntaxSingleError).type) {
+                errorType = err.type;
+              }
+
+              let msg = `${errorType} error`;
+              if (!err.metadata || err.type) {
+                msg += ` at line=${err.line?.start}, column=${err.column?.start}`;
+              }
+              msg += `: ${err.msg}`;
+              return msg;
+            })
+            .join("\n\t* ")}\n\n`,
+        );
+
+        for (let index = 0; index < errorsCount; index++) {
+          const expected = testCase.expected_errors[index];
+          expected.metadata!.module = "test";
+          // We're asserting an error type against an JSON object here, it works but isn't type correct
+          // @ts-expect-error
+          expect(exception.errors[index]).toMatchObject(expected);
+        }
+      }
+    });
+  });
+
   it("should allow passing a custom schema version", () => {
     const model = transformModuleFilesToModel(
       [
@@ -68,4 +122,8 @@ describe("transformModuleFilesToModel", () => {
 
     expect(model.schema_version).toEqual("1.1");
   });
+});
+
+describe("transformModuleFilesToModel - semantic validation test cases", () => {
+  
 });

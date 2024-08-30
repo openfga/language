08715ffcfde3ab16d81526beb718f5b1308cddf2
Raghd Hamzeh <raghd.hamzeh@openfga.dev>
2024-08-30 09:15:43 -0400

    chore(java): removed redundant validation code

--- pkg/java/src/main/java/dev/openfga/language/validation/ModelValidator.java
+++ pkg/java/src/main/java/dev/openfga/language/validation/ModelValidator.java
@@ -371,22 +371,6 @@ public class ModelValidator {
             case TupleToUserset: {
                 if (childDef.getFrom() != null && childDef.getTarget() != null) {
 
-                    var currentRelation = relations.get(relationName);
-                    if (currentRelation != null && currentRelation.getTupleToUserset() != null) {
-                        var tupleset = currentRelation
-                                .getTupleToUserset()
-                                .getTupleset()
-                                .getRelation();
-                        if (tupleset != null
-                                && relations.containsKey(tupleset)
-                                && relations.get(tupleset).getThis() == null) {
-                            var typeIndex = dsl.getTypeLineNumber(typeName);
-                            var lineIndex = dsl.getRelationLineNumber(relationName, typeIndex);
-                            errors.raiseTupleUsersetRequiresDirect(lineIndex, childDef.getFrom());
-                            break;
-                        }
-                    }
-
                     if (!relations.containsKey(childDef.getFrom())) {
                         var typeIndex = dsl.getTypeLineNumber(typeName);
                         var lineIndex = dsl.getRelationLineNumber(relationName, typeIndex);

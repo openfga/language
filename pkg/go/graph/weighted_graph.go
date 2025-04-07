cffffe4a9efc9ba7a8d4376c8310957656c84198
yissellokta <146273812+yissellokta@users.noreply.github.com>
2025-04-07 14:59:41 -0400

    removing dependency on drawing graph (#441)
    * removing dependency on drawing graph
    
    * remove build using the AuthorizationModelGraph as input
    
    * fix lint errors
    
    * lint fix
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>
    
    * Update pkg/go/graph/weighted_graph.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * Update pkg/go/graph/weighted_graph_builder.go
    
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>
    
    * address review comments
    
    * fix wrong string
    
    * added new tests for tuple cycle
    
    * Update pkg/go/graph/weighted_graph_builder_test.go
    
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>
    
    * removing initial wildcards creation
    
    ---------
    
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>
    Co-authored-by: Joshua Jones <joshua.jones@okta.com>

--- pkg/go/graph/weighted_graph.go
+++ pkg/go/graph/weighted_graph.go
@@ -50,22 +50,76 @@ func NewWeightedAuthorizationModelGraph() *WeightedAuthorizationModelGraph {
 
 // AddNode adds a node to the graph with optional nodeType and weight.
 func (wg *WeightedAuthorizationModelGraph) AddNode(uniqueLabel, label string, nodeType NodeType) {
-	wildcards := make([]string, 0)
+	var wildcards []string
 	if nodeType == SpecificTypeWildcard {
-		wildcards = append(wildcards, uniqueLabel[:len(uniqueLabel)-2])
+		wildcards = []string{uniqueLabel[:len(uniqueLabel)-2]}
 	}
 	wg.nodes[uniqueLabel] = &WeightedAuthorizationModelNode{uniqueLabel: uniqueLabel, label: label, nodeType: nodeType, wildcards: wildcards}
 }
 
+// AddNode adds a node to the graph with optional nodeType and weight.
+func (wg *WeightedAuthorizationModelGraph) GetOrAddNode(uniqueLabel, label string, nodeType NodeType) *WeightedAuthorizationModelNode {
+	if existingNode := wg.nodes[uniqueLabel]; existingNode != nil {
+		return existingNode
+	}
+	var wildcards []string
+	if nodeType == SpecificTypeWildcard {
+		wildcards = []string{uniqueLabel[:len(uniqueLabel)-2]}
+	}
+	wg.nodes[uniqueLabel] = &WeightedAuthorizationModelNode{uniqueLabel: uniqueLabel, label: label, nodeType: nodeType, wildcards: wildcards}
+	return wg.nodes[uniqueLabel]
+}
+
 func (wg *WeightedAuthorizationModelGraph) AddEdge(fromID, toID string, edgeType EdgeType, tuplesetRelation string, conditions []string) {
-	wildcards := make([]string, 0)
 	fromNode := wg.nodes[fromID]
 	toNode := wg.nodes[toID]
 	if len(conditions) == 0 {
 		conditions = []string{NoCond}
 	}
-	edge := &WeightedAuthorizationModelEdge{from: fromNode, to: toNode, edgeType: edgeType, tuplesetRelation: tuplesetRelation, wildcards: wildcards, conditions: conditions}
+	edge := &WeightedAuthorizationModelEdge{from: fromNode, to: toNode, edgeType: edgeType, tuplesetRelation: tuplesetRelation, wildcards: nil, conditions: conditions}
 	wg.edges[fromID] = append(wg.edges[fromID], edge)
+}
+
+func (wg *WeightedAuthorizationModelGraph) UpsertEdge(fromNode, toNode *WeightedAuthorizationModelNode, edgeType EdgeType, tuplesetRelation string, condition string) error {
+	if fromNode == nil || toNode == nil {
+		return fmt.Errorf("%w: Model cannot be parsed", ErrInvalidModel)
+	}
+
+	if condition == "" {
+		condition = NoCond
+	}
+
+	edges := wg.edges[fromNode.uniqueLabel]
+	for _, edge := range edges {
+		if edge.to.uniqueLabel == toNode.uniqueLabel && edge.edgeType == edgeType && edge.tuplesetRelation == tuplesetRelation {
+			for _, cond := range edge.conditions {
+				if cond == condition {
+					return nil
+				}
+			}
+			edge.conditions = append(edge.conditions, condition)
+			return nil
+		}
+	}
+
+	conditions := []string{condition}
+	edge := &WeightedAuthorizationModelEdge{from: fromNode, to: toNode, edgeType: edgeType, tuplesetRelation: tuplesetRelation, wildcards: nil, conditions: conditions}
+	wg.edges[fromNode.uniqueLabel] = append(wg.edges[fromNode.uniqueLabel], edge)
+	return nil
+}
+
+func (wg *WeightedAuthorizationModelGraph) HasEdge(fromNode, toNode *WeightedAuthorizationModelNode, edgeType EdgeType, tuplesetRelation string) bool {
+	if fromNode == nil || toNode == nil {
+		return false
+	}
+	edges := wg.edges[fromNode.uniqueLabel]
+	for _, edge := range edges {
+		if edge.to.uniqueLabel == toNode.uniqueLabel && edge.edgeType == edgeType && edge.tuplesetRelation == tuplesetRelation {
+			return true
+		}
+	}
+
+	return false
 }
 
 // AssignWeights assigns weights to all the edges and nodes of the graph.
@@ -91,18 +145,33 @@ func (wg *WeightedAuthorizationModelGraph) AssignWeights() error {
 }
 
 func (wg *WeightedAuthorizationModelGraph) calculateEdgeWildcards(edge *WeightedAuthorizationModelEdge) {
+	// if wildcards already exist, we don't need to calculate them
 	if len(edge.wildcards) > 0 {
 		return
 	}
 	nodeWildcards := wg.nodes[edge.to.uniqueLabel].wildcards
+	// if the node does not have any wildcards, we don't need to calculate them
 	if len(nodeWildcards) == 0 {
 		return
 	}
-	edge.wildcards = append(edge.wildcards, nodeWildcards...)
+	// otherwise add the node wildcards
+	edge.wildcards = nodeWildcards
 }
 
 func (wg *WeightedAuthorizationModelGraph) addReferentialWildcardsToEdge(edge *WeightedAuthorizationModelEdge, referentialNodeID string) {
 	referentialNode := wg.nodes[referentialNodeID]
+	// if the node does not have any wildcards, we don't need to calculate them
+	if len(referentialNode.wildcards) == 0 {
+		return
+	}
+	// if the edge does not have any wildcards, we can add the referential node wildcards
+	if len(edge.wildcards) == 0 {
+		edge.wildcards = referentialNode.wildcards
+		return
+	}
+	// otherwise add the referential node wildcards to the existing edge wildcards only if the wildcard does not exist in the slice
+	// this is to avoid duplicates
+	// and to ensure that the wildcards are unique
 	for _, wildcard := range referentialNode.wildcards {
 		if !slices.Contains(edge.wildcards, wildcard) {
 			edge.wildcards = append(edge.wildcards, wildcard)
@@ -113,6 +182,14 @@ func (wg *WeightedAuthorizationModelGraph) addReferentialWildcardsToEdge(edge *W
 func (wg *WeightedAuthorizationModelGraph) addReferentialWildcardsToNode(nodeID string, referentialNodeID string) {
 	referentialNode := wg.nodes[referentialNodeID]
 	node := wg.nodes[nodeID]
+	// if the node does not have any wildcards, we can add the referential node wildcards
+	if len(node.wildcards) == 0 {
+		node.wildcards = referentialNode.wildcards
+		return
+	}
+	// otherwise add the referential node wildcards to the existing node wildcards only if the wildcard does not exist in the slice
+	// this is to avoid duplicates
+	// and to ensure that the wildcards are unique
 	for _, wildcard := range referentialNode.wildcards {
 		if !slices.Contains(node.wildcards, wildcard) {
 			node.wildcards = append(node.wildcards, wildcard)
@@ -126,10 +203,29 @@ func (wg *WeightedAuthorizationModelGraph) addEdgeWildcardsToNode(nodeID string,
 		return
 	}
 
+	// if the node does not have any wildcards, we can add the edge wildcards
+	if len(node.wildcards) == 0 {
+		node.wildcards = edge.wildcards
+		return
+	}
+	// otherwise add the edge wildcards to the existing node wildcards only if the wildcard does not exist in the slice
+	// this is to avoid duplicates
+	// and to ensure that the wildcards are unique
 	for _, wildcard := range edge.wildcards {
 		if !slices.Contains(node.wildcards, wildcard) {
 			node.wildcards = append(node.wildcards, wildcard)
 		}
+	}
+}
+
+func (wg *WeightedAuthorizationModelGraph) addWildcardToEdge(wildcardType string, edge *WeightedAuthorizationModelEdge) {
+	if len(edge.wildcards) == 0 {
+		edge.wildcards = []string{wildcardType}
+		return
+	}
+
+	if !slices.Contains(edge.wildcards, wildcardType) {
+		edge.wildcards = append(edge.wildcards, wildcardType)
 	}
 }
 
@@ -157,7 +253,7 @@ func (wg *WeightedAuthorizationModelGraph) calculateNodeWeight(nodeID string, vi
 			uniqueLabel := edge.to.uniqueLabel
 			if edge.to.nodeType == SpecificTypeWildcard {
 				uniqueLabel = uniqueLabel[:len(uniqueLabel)-2]
-				edge.wildcards = append(edge.wildcards, uniqueLabel)
+				wg.addWildcardToEdge(uniqueLabel, edge)
 				wg.addEdgeWildcardsToNode(nodeID, edge)
 			}
 			edge.weights[uniqueLabel] = 1
@@ -278,7 +374,7 @@ func (wg *WeightedAuthorizationModelGraph) calculateNodeWeightFromTheEdges(nodeI
 		return tupleCycles, nil
 	}
 
-	// recursive case
+	// tuple cycle where the node is responsible for the cycle and it needs to fix the cycle
 	if node.nodeType == SpecificTypeAndRelation && wg.isNodeTupleCycleReference(nodeID, tupleCycles) {
 		// calculate the weight of the node and fix all the dependencies that are in the tuple cycle.
 		err := wg.calculateNodeWeightAndFixDependencies(nodeID, tupleCycleDependencies)
@@ -289,6 +385,7 @@ func (wg *WeightedAuthorizationModelGraph) calculateNodeWeightFromTheEdges(nodeI
 		return tupleCycles, nil
 	}
 
+	// there is a cycle but the node is not responsible for the cycle and it is not an operator node
 	if node.nodeType != OperatorNode {
 		// even when there is a cycle, if the relation is not recursive then we calculate the weight using the max strategy
 		err = wg.calculateNodeWeightWithMaxStrategy(nodeID)
@@ -359,6 +456,7 @@ func (wg *WeightedAuthorizationModelGraph) calculateNodeWeightWithEnforceTypeStr
 	}
 
 	for _, edge := range edges {
+		// for but not ensure that the first edge is the left edge
 		// the first time, take the weights of the edge
 		if len(weights) == 0 {
 			for key, value := range edge.weights {
--- pkg/go/graph/weighted_graph_builder.go
+++ pkg/go/graph/weighted_graph_builder.go
@@ -1,8 +1,11 @@
 package graph
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 
+	"github.com/oklog/ulid/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/multi"
@@ -18,60 +21,178 @@ func NewWeightedAuthorizationModelGraphBuilder() *WeightedAuthorizationModelGrap
 }
 
 func (wgb *WeightedAuthorizationModelGraphBuilder) Build(model *openfgav1.AuthorizationModel) (*WeightedAuthorizationModelGraph, error) {
-	g, err := NewAuthorizationModelGraph(model)
-	if err != nil {
-		return nil, err
-	}
-	g, err = g.Reversed() // we want edges to have the direction of Check when doing the weight assignments later
-	if err != nil {
-		return nil, err
-	}
-
 	wb := NewWeightedAuthorizationModelGraph()
-	// Add all nodes
-	iterNodes := g.Nodes()
-	for iterNodes.Next() {
-		nextNode := iterNodes.Node()
-		node, ok := nextNode.(*AuthorizationModelNode)
-		if !ok {
-			return nil, fmt.Errorf("%w: could not cast to WeightedAuthorizationModelNode", ErrBuildingGraph)
+	// sort types by name to guarantee stable output
+	sortedTypeDefs := make([]*openfgav1.TypeDefinition, len(model.GetTypeDefinitions()))
+	copy(sortedTypeDefs, model.GetTypeDefinitions())
+
+	slices.SortFunc(sortedTypeDefs, func(a, b *openfgav1.TypeDefinition) int {
+		return cmp.Compare(a.GetType(), b.GetType())
+	})
+
+	for _, typeDef := range sortedTypeDefs {
+		wb.GetOrAddNode(typeDef.GetType(), typeDef.GetType(), SpecificType)
+
+		// sort relations by name to guarantee stable output
+		sortedRelations := make([]string, 0, len(typeDef.GetRelations()))
+		for relationName := range typeDef.GetRelations() {
+			sortedRelations = append(sortedRelations, relationName)
 		}
 
-		wb.AddNode(node.uniqueLabel, node.label, node.nodeType)
-	}
+		slices.Sort(sortedRelations)
 
-	// Add all the edges
-	iterEdges := g.Edges()
-	for iterEdges.Next() {
-		nextEdge, ok := iterEdges.Edge().(multi.Edge)
-		if !ok {
-			return nil, fmt.Errorf("%w: could not cast %v to multi.Edge", ErrBuildingGraph, iterEdges.Edge())
-		}
-
-		// NOTE: because we use a multigraph, one edge can include multiple lines, so we need to add each line individually.
-		iterLines := nextEdge.Lines
-		for iterLines.Next() {
-			nextLine := iterLines.Line()
-			castedEdge, ok := nextLine.(*AuthorizationModelEdge)
-			if !ok {
-				return nil, fmt.Errorf("%w: could not cast %v to AuthorizationModelEdge", ErrBuildingGraph, nextLine)
+		for _, relation := range sortedRelations {
+			uniqueLabel := typeDef.GetType() + "#" + relation
+			parentNode := wb.GetOrAddNode(uniqueLabel, uniqueLabel, SpecificTypeAndRelation)
+			rewrite := typeDef.GetRelations()[relation]
+			err := wgb.parseRewrite(wb, parentNode, model, rewrite, typeDef, relation)
+			if err != nil {
+				return nil, err
 			}
-			castedFromNode, ok := castedEdge.From().(*AuthorizationModelNode)
-			if !ok {
-				return nil, fmt.Errorf("%w: could not cast %v to AuthorizationModelNode", ErrBuildingGraph, castedEdge.From())
-			}
-			castedToNode, ok := castedEdge.To().(*AuthorizationModelNode)
-			if !ok {
-				return nil, fmt.Errorf("%w: could not cast %v to AuthorizationModelNode", ErrBuildingGraph, castedEdge.To())
-			}
-			wb.AddEdge(castedFromNode.uniqueLabel, castedToNode.uniqueLabel, castedEdge.edgeType, castedEdge.tuplesetRelation, castedEdge.conditions)
 		}
 	}
 
-	err = wb.AssignWeights()
+	err := wb.AssignWeights()
 	if err != nil {
 		return nil, err
 	}
 
 	return wb, nil
+}
+
+func (wgb *WeightedAuthorizationModelGraphBuilder) parseRewrite(wg *WeightedAuthorizationModelGraph, parentNode *WeightedAuthorizationModelNode, model *openfgav1.AuthorizationModel, rewrite *openfgav1.Userset, typeDef *openfgav1.TypeDefinition, relation string) error {
+	var operator string
+
+	var children []*openfgav1.Userset
+
+	switch rw := rewrite.GetUserset().(type) {
+	case *openfgav1.Userset_This:
+		return wgb.parseThis(wg, parentNode, typeDef, relation)
+	case *openfgav1.Userset_ComputedUserset:
+		wgb.parseComputed(wg, parentNode, typeDef, rw.ComputedUserset.GetRelation())
+		return nil
+	case *openfgav1.Userset_TupleToUserset:
+		return wgb.parseTupleToUserset(wg, parentNode, model, typeDef, rw.TupleToUserset)
+	case *openfgav1.Userset_Union:
+		operator = UnionOperator
+		children = rw.Union.GetChild()
+
+	case *openfgav1.Userset_Intersection:
+		operator = IntersectionOperator
+		children = rw.Intersection.GetChild()
+
+	case *openfgav1.Userset_Difference:
+		operator = ExclusionOperator
+		children = []*openfgav1.Userset{
+			rw.Difference.GetBase(),
+			rw.Difference.GetSubtract(),
+		}
+	}
+
+	operatorNodeName := operator + ":" + ulid.Make().String()
+	operatorNode := wg.GetOrAddNode(operatorNodeName, operator, OperatorNode)
+
+	// add one edge "relation" -> "operation that defined the operator"
+	// Note: if this is a composition of operators, operationNode will be nil and this edge won't be added.
+	wg.AddEdge(parentNode.GetUniqueLabel(), operatorNodeName, RewriteEdge, "", nil)
+	for _, child := range children {
+		err := wgb.parseRewrite(wg, operatorNode, model, child, typeDef, relation)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (wgb *WeightedAuthorizationModelGraphBuilder) parseTupleToUserset(wg *WeightedAuthorizationModelGraph, parentNode *WeightedAuthorizationModelNode, model *openfgav1.AuthorizationModel, typeDef *openfgav1.TypeDefinition, rewrite *openfgav1.TupleToUserset) error {
+	// e.g. define viewer: admin from parent
+	// "parent" is the tupleset
+	tuplesetRelation := rewrite.GetTupleset().GetRelation()
+	// "admin" is the computed relation
+	computedRelation := rewrite.GetComputedUserset().GetRelation()
+
+	// find all the directly related types to the tupleset
+	relationMetadata, ok := typeDef.GetMetadata().GetRelations()[tuplesetRelation]
+	if !ok {
+		return fmt.Errorf("%w: Model cannot be parsed. %s invalid tupleset relation", ErrInvalidModel, tuplesetRelation)
+	}
+	directlyRelated := relationMetadata.GetDirectlyRelatedUserTypes()
+	if len(directlyRelated) == 0 {
+		return fmt.Errorf("%w: Model cannot be parsed. No type and relation link exists for tupleset relation %s and computed relation %s", ErrInvalidModel, tuplesetRelation, computedRelation)
+	}
+
+	for _, relatedType := range directlyRelated {
+		tuplesetType := relatedType.GetType()
+
+		if !typeAndRelationExists(model, tuplesetType, computedRelation) {
+			return fmt.Errorf("%w: Model cannot be parsed. %s type does not have defined %s relation", ErrInvalidModel, tuplesetType, computedRelation)
+		}
+
+		rewrittenNodeName := fmt.Sprintf("%s#%s", tuplesetType, computedRelation)
+		nodeSource := wg.GetOrAddNode(rewrittenNodeName, rewrittenNodeName, SpecificTypeAndRelation)
+		typeTuplesetRelation := typeDef.GetType() + "#" + tuplesetRelation
+
+		if wg.HasEdge(parentNode, nodeSource, TTUEdge, typeTuplesetRelation) {
+			// we don't need to do any condition update, only de-dup the edge. In case of TTU
+			// the direct relation will have the conditions
+			// for example, in the case of
+			// type group
+			//   relations
+			// 		define rel1: [user] or rel1 from parent
+			//		define parent: [group, group with condX]
+			// In the graph we only have one TTU edge from the OR node to the group#rel1 node, but there are no conditions associated to it
+			// the conditions are associated to the edge from group#parent node to the group node. This direct edge has two conditions: none and condX
+			continue
+		}
+
+		// new edge from "xxx#admin" to "yyy#viewer" tuplesetRelation on "yyy#parent"
+		wg.UpsertEdge(parentNode, nodeSource, TTUEdge, typeTuplesetRelation, relatedType.GetCondition())
+	}
+	return nil
+}
+
+func (wgb *WeightedAuthorizationModelGraphBuilder) parseComputed(wg *WeightedAuthorizationModelGraph, parentNode *WeightedAuthorizationModelNode, typeDef *openfgav1.TypeDefinition, relation string) {
+	nodeType := RewriteEdge
+	// e.g. define x: y. Here y is the rewritten relation
+	rewrittenNodeName := typeDef.GetType() + "#" + relation
+	newNode := wg.GetOrAddNode(rewrittenNodeName, rewrittenNodeName, SpecificTypeAndRelation)
+	// new edge from x to y
+	if parentNode.nodeType == SpecificTypeAndRelation && newNode.nodeType == SpecificTypeAndRelation {
+		nodeType = ComputedEdge
+	}
+	wg.AddEdge(parentNode.uniqueLabel, newNode.uniqueLabel, nodeType, "", nil)
+}
+
+func (wgb *WeightedAuthorizationModelGraphBuilder) parseThis(wg *WeightedAuthorizationModelGraph, parentNode *WeightedAuthorizationModelNode, typeDef *openfgav1.TypeDefinition, relation string) error {
+	var directlyRelated []*openfgav1.RelationReference
+	var curNode *WeightedAuthorizationModelNode
+
+	if relationMetadata, ok := typeDef.GetMetadata().GetRelations()[relation]; ok {
+		directlyRelated = relationMetadata.GetDirectlyRelatedUserTypes()
+	}
+
+	for _, directlyRelatedDef := range directlyRelated {
+		switch {
+		case directlyRelatedDef.GetRelationOrWildcard() == nil:
+			// direct assignment to concrete type
+			assignableType := directlyRelatedDef.GetType()
+			curNode = wg.GetOrAddNode(assignableType, assignableType, SpecificType)
+		case directlyRelatedDef.GetWildcard() != nil:
+			// direct assignment to wildcard
+			assignableWildcard := directlyRelatedDef.GetType() + ":*"
+			curNode = wg.GetOrAddNode(assignableWildcard, assignableWildcard, SpecificTypeWildcard)
+		default:
+			// direct assignment to userset
+			assignableUserset := directlyRelatedDef.GetType() + "#" + directlyRelatedDef.GetRelation()
+			curNode = wg.GetOrAddNode(assignableUserset, assignableUserset, SpecificTypeAndRelation)
+		}
+
+		// de-dup types that are conditioned, e.g. if define viewer: [user, user with condX]
+		// we only draw one edge from user to x#viewer, but with two conditions: none and condX
+		err := wg.UpsertEdge(parentNode, curNode, DirectEdge, "", directlyRelatedDef.GetCondition())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
